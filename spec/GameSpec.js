89a5963aa79d007a6973a5d6598de27d007adacf
Jordan Roberts <jordanroberts@talk21.com>
2019-05-13 13:25:08 +0100

    Can calculate a spare

--- spec/GameSpec.js
+++ spec/GameSpec.js
@@ -8,13 +8,21 @@ describe('Game', function(){
 
   it('can calculate a gutter game', function(){
     rollMany(20, 0)
-    expect(game.score).toBe(0)
+    expect(game.score()).toBe(0)
   });
 
   it('can roll all ones', function(){
     rollMany(20, 1)
-    expect(game.score).toBe(20)
+    expect(game.score()).toBe(20)
   });
+
+  it('can roll a spare', function(){
+    game.roll(5);
+    game.roll(5);
+    game.roll(3);
+    rollMany(17,0);
+    expect(game.score()).toBe(16)
+  })
 
   function rollMany(rolls, pins){
     for(var i = 0; i < rolls; i ++) {
--- src/game.js
+++ src/game.js
@@ -1,11 +1,23 @@
 function Game (){
-  this.score = 0
+  this.rolls = [ ]
+  this.currentRoll = 0
 };
 
 Game.prototype.roll = function(pins) {
-  return this.score += pins
-}
+  this.rolls[this.currentRoll++] = pins;
+};
 
 Game.prototype.score = function(){
-  return this.score
+  var score = 0
+  var i = 0
+  for (var frame = 0; frame < 10; frame++) {
+    if(this.rolls[i] + this.rolls[i + 1] == 10){
+      score += 10 + this.rolls[i+2];
+      i += 2;
+    } else {
+  score += this.rolls[i] + this.rolls[i + 1]
+  i += 2
+  }
+}
+  return score
 }
