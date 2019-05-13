79e458ef3dda3133652128342f20c67997ce13d7
Jordan Roberts <jordanroberts@talk21.com>
2019-05-13 13:45:17 +0100

    Tidied up code in the score function

--- src/game.js
+++ src/game.js
@@ -8,26 +8,36 @@ Game.prototype.roll = function(pins) {
 };
 
 Game.prototype.score = function(){
-  var score = 0
-  var frameIndex = 0
+  var score = 0;
+  var frameIndex = 0;
   for (var frame = 0; frame < 10; frame++) {
-    if(this.rolls[frameIndex] == 10)
+    if (this.rolls[frameIndex] == 10) // strike
     {
-      score += 10 + this.rolls[frameIndex+1] + this.rolls[frameIndex+2]
+      score += 10 + this._strikeBonus(frameIndex);
       frameIndex++;
-    }
-    else if(this._isSpare(frameIndex))
-    {
-      score += 10 + this.rolls[frameIndex+2];
+    } else if(this._isSpare(frameIndex)) {
+      score += 10 + this._spareBonus(frameIndex);
       frameIndex += 2;
     } else {
-  score += this.rolls[frameIndex] + this.rolls[frameIndex + 1]
-  frameIndex += 2
+      score += this._totalBallsInFrame(frameIndex);
+      frameIndex += 2
+    }
   }
-}
   return score
 }
 
 Game.prototype._isSpare = function(frameIndex) {
   return this.rolls[frameIndex] + this.rolls[frameIndex + 1] === 10
+}
+
+Game.prototype._totalBallsInFrame = function(frameIndex) {
+  return this.rolls[frameIndex] + this.rolls[frameIndex + 1]
+}
+
+Game.prototype._spareBonus = function(frameIndex){
+  return this.rolls[frameIndex + 2]
+}
+
+Game.prototype._strikeBonus = function(frameIndex){
+  return this.rolls[frameIndex + 1] + this.rolls[frameIndex +2]
 }
